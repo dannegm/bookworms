cea72585615d0c5fbb6d4feb536d2d08dea0554f
dannegm <im@danielgarcia.me>
2024-12-09 13:07:07 -0600

    Desactivar descarga de libros

--- src/components/layout/root-layout.jsx
+++ src/components/layout/root-layout.jsx
@@ -15,6 +15,11 @@ export default function RootLayout({ environmentInfo, children }) {
             <body className={cn(noto.className, 'antialiased')}>
                 <Providers environmentInfo={environmentInfo}>
                     <Debugger />
+
+                    <div className='bg-yellow-100 text-yellow-800 p-4 border border-yellow-300 rounded-md text-center'>
+                        La descarga de libros estará deshabilitada del 9 de diciembre hasta el 20 de
+                        diciembre.
+                    </div>
                     {children}
                 </Providers>
             </body>
