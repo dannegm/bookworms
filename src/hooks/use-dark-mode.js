89901108488e15aeadc6584d50ddeb7a1824668c
dannegm <im@danielgarcia.me>
2026-06-10 13:03:50 -0600

    fix: update local storage key for theme management

--- src/hooks/use-dark-mode.js
+++ src/hooks/use-dark-mode.js
@@ -2,7 +2,7 @@ import { useLocalStorage } from '@/hooks/use-local-storage';
 
 export const useDarkMode = () => {
     // const getSystemTheme = () => window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    const [theme, setTheme] = useLocalStorage('theme', 'light');
+    const [theme, setTheme] = useLocalStorage('settings:theme', 'light');
     const toggle = () => setTheme(prev => (prev === 'dark' ? 'light' : 'dark'));
     return [theme, toggle];
 };
